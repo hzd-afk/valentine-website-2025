e746b32bf4dc0e2556c5f4aa54d8c353ce8b6d85
Abhinav Mahato <abhinavmahato1@gmail.com>
2026-02-08 23:57:36 +0530

    Update music streaming URL in config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://audio.com/jackson-1823279981992976/audio/perfect-ed-sheeran", // Music streaming URL
+        musicUrl: "https://open.spotify.com/embed/track/0tgVpDi06FyKpA1z0VMD4v" // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
